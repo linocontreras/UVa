c599c43152f7589c3201f5f5e187d8afb84ff2a3
Lino Contreras <linocontreras@outlook.com>
2017-11-09 10:25:20 -0600

    Added 10041

---
+++ 10041/10041.c
@@ -0,0 +1,26 @@
+/**---------------------------------------------------------
+ * 
+ * Actividad de programación: Problema 11059 de UVA
+ * Fecha: 09-Nov-2017
+ * Autor: A01700457 - Lino Contreras
+ * 
+ * ---------------------------------------------------------*/
+
+#include<stdio.h>
+
+int main(int argc, char** argv){
+    int t, r;
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d", &r);
+        if(r==0){
+            printf("0\n");
+            continue;
+        }
+        scanf("%d", &);
+        while(--r){
+            scanf("%d", &);
+        }
+    }
+    return 0;
+}
